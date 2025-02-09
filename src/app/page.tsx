b7f84003bbab9ffde36d85b9ba43c39522efcb6a
Shafa Naura Priesha Ardhynindya <shafanaura48@gmail.com>
2025-02-09 18:50:12 +0700

    fix: params

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import HomeScreen from "@/screens/home";
 import { getMessages } from "next-intl/server";
 
-export async function generateMetadata({
-  params: { locale },
-}: {
-  params: { locale: string };
-}) {
+type Params = Promise<{ locale: string }>;
+
+export async function generateMetadata(props: { params: Params }) {
+  const { locale } = await props.params;
+
   const messages: any = await getMessages({ locale });
   const title = messages.Metadata.title;
   return { title };
